43988af78be4575e1be7a0becfe5ae245bef3455
Дмитрий Галашев <galashev1990@gmail.com>
2023-01-15 21:59:15 +0300

    Adding Code

--- People/src/main/java/Person.java
+++ People/src/main/java/Person.java
@@ -1,8 +1,11 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
 public class Person {
     protected final String name;
     protected final String surname;
-    protected final int age;
-    protected final String adress;
+    protected int age;
+    protected String address;
 
 
     public Person(String name, String surname) {
@@ -17,22 +20,64 @@ public class Person {
 
     }
 
-    public boolean hasAge() { /*...*/ }
-    public boolean hasAddress() { /*...*/ }
+    public boolean hasAge() {
+        return age >= 0;
+    }
 
-    public String getName() { /*...*/ }
-    public String getSurname() { /*...*/ }
-    public OptionalInt getAge() { /*...*/ }
-    public String getAddress() { /*...*/ }
+    public boolean hasAddress() {
+        return address != null;
+    }
 
-    public void setAddress(String address) { /*...*/ }
-    public void happyBirthday() { /*...*/ }
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String adress) {
+        this.address = adress;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) age++;
+    }
 
     @Override
-    public String toString() { /*...*/ }
+    public String toString() {
+        return "Person{" +
+            "name='" + name + '\'' +
+            ", surname='" + surname + '\'' +
+            ", age=" + age +
+            ", address='" + address + '\'' +
+            '}';
+    }
 
     @Override
-    public int hashCode() { /*...*/ }
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 
-    public PersonBuilder newChildBuilder() { /*...*/ }
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAddress(address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name) && surname.equals(person.surname) && Objects.equals(address, person.address);
+    }
+
+
 }
--- People/src/main/java/PersonBuilder.java
+++ People/src/main/java/PersonBuilder.java
@@ -1,9 +1,43 @@
 public class PersonBuilder {
-    public PersonBuilder setName(String name) { /*...*/ }
-    public PersonBuilder setSurname(String surname) { /*...*/ }
-    public PersonBuilder setAge(int age) { /*...*/ }
-    public PersonBuilder setAddress(String address) { /*...*/ }
+    private String name;
+    private String surname;
+    private int age = -1;
+    private String address;
 
-    public Person build() { /*...*/ }
+    public PersonBuilder setName(String name)  {
+        if (name == null || name.isEmpty()) {
+        throw new IllegalArgumentException("Вы не указали имя.");
+    } else this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Вы не указали фамилию.");
+        } else this.surname = surname;
+        return this;
+    }
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Вы указали не корректный возраст.");
+        } else this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build()  {
+        Person person;
+        if (name == null || surname == null)
+            throw new IllegalStateException("Вы указали не все данные " + name + " " + surname);
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else person = new Person(name, surname, age);
+        person.setAddress(address);
+        return person;
+    }
 
 }
